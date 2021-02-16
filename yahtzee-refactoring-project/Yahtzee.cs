a1feaa8614c963cf467993b2c870ade580fb7fc7
Yaakov Staum <yakstaum@gmail.com>
2021-02-16 17:13:02 +0200

    finding total in Chance refactoring

--- yahtzee-refactoring-project/Yahtzee.cs
+++ yahtzee-refactoring-project/Yahtzee.cs
@@ -4,12 +4,7 @@
     {
         public static int Chance(int d1, int d2, int d3, int d4, int d5)
         {
-            int total = 0;
-            total += d1;
-            total += d2;
-            total += d3;
-            total += d4;
-            total += d5;
+            int total = d1 + d2 + d3 + d4 + d5;
             return total;
         }
 
